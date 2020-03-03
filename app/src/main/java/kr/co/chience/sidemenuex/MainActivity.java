347b215de40a2215f5e244b42515134820606b2a
lareina-Ipse <lareina.ipse@gmail.com>
2020-03-03 17:11:25 +0900

    클릭 시 해당 fragment로 이동이 안됨.

--- app/src/main/java/kr/co/chience/sidemenuex/MainActivity.java
+++ app/src/main/java/kr/co/chience/sidemenuex/MainActivity.java
@@ -1,14 +1,55 @@
 package kr.co.chience.sidemenuex;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private AppBarConfiguration appBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+
+        appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.home, R.id.party, R.id.dance)
+                .setDrawerLayout(drawerLayout)
+                .build();
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+
+
+    }
+
+
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this ,R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.home:
+
+                break;
+
+        }
     }
 }
