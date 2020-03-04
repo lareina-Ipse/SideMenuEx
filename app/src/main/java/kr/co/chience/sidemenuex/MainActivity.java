1d296eed243c50e779996d3cfad8f3f72fb5c64b
lareina-Ipse <lareina.ipse@gmail.com>
2020-03-04 13:41:07 +0900

    side menu 구현완료

--- app/src/main/java/kr/co/chience/sidemenuex/MainActivity.java
+++ app/src/main/java/kr/co/chience/sidemenuex/MainActivity.java
@@ -1,55 +1,87 @@
 package kr.co.chience.sidemenuex;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
-import android.view.View;
+import android.view.MenuItem;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import kr.co.chience.sidemenuex.ui.DashboardFragment;
+import kr.co.chience.sidemenuex.ui.EventFragment;
+import kr.co.chience.sidemenuex.ui.SearchFragment;
 
-    private AppBarConfiguration appBarConfiguration;
+public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToogle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
-
-        appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.home, R.id.party, R.id.dance)
-                .setDrawerLayout(drawerLayout)
-                .build();
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-
-
-    }
-
-
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this ,R.id.nav_host_fragment);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
+        mDrawerLayout = findViewById(R.id.drawer);
+        NavigationView nv = findViewById(R.id.nav_view);
+        mToogle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+        mDrawerLayout.addDrawerListener(mToogle);
+        mToogle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setupDrawerContent(nv);
     }
 
     @Override
-    public void onClick(View v) {
-        switch(v.getId()) {
-            case R.id.home:
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (mToogle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
+    public void selectIterDrawer(MenuItem menuItem) {
+        androidx.fragment.app.Fragment myFragment = null;
+        Class fragmentClass;
+        switch (menuItem.getItemId()) {
+            case R.id.db:
+                fragmentClass = DashboardFragment.class;
+                break;
+            case R.id.event:
+                fragmentClass = EventFragment.class;
                 break;
 
+            case R.id.search:
+                fragmentClass = SearchFragment.class;
+                break;
+            default:
+                fragmentClass = DashboardFragment.class;
+
         }
+
+        try {
+            myFragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.content, myFragment).commit();
+        menuItem.setChecked(true);
+        setTitle(menuItem.getTitle());
+        mDrawerLayout.closeDrawers();
     }
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                selectIterDrawer(item);
+                return true;
+            }
+        });
+    }
+
 }
--- app/src/main/java/kr/co/chience/sidemenuex/ui/DashboardFragment.java
+++ app/src/main/java/kr/co/chience/sidemenuex/ui/DashboardFragment.java
@@ -14,10 +14,10 @@ import kr.co.chience.sidemenuex.R;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class HomeFragment extends Fragment {
+public class DashboardFragment extends Fragment {
 
 
-    public HomeFragment() {
+    public DashboardFragment() {
         // Required empty public constructor
     }
 
@@ -26,7 +26,7 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        return inflater.inflate(R.layout.fragment_dashboard, container, false);
     }
 
 }
